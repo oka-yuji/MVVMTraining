44a05e0a489c8edad39fd71214e907472bae0769
岡優志 <yu.j.0513@gmail.com>
2021-05-29 11:02:14 +0900

    calculateModelをsturctからclassに変換しました

--- MVVMTraining/MVVMTraining/Model/CalculateModel.swift
+++ MVVMTraining/MVVMTraining/Model/CalculateModel.swift
@@ -7,13 +7,12 @@
 
 import Foundation
 
-struct CalculateModel{
-   private var num1:Int
-   private var num2:Int
-   private var multiplication:Int
-}
-///掛け算をするfunc num1,num2はInt型
-func calculate(num1:Int,num2:Int) -> Int {
-    let multiplication = num1 * num2
-    return multiplication
+class CalculateModel{
+    private var num1:Int = 0
+    private var num2:Int = 0
+    ///掛け算をするfunc num1,num2はInt型
+    func calculate(num1:Int,num2:Int) -> Int {
+        let multiplication = num1 * num2
+        return multiplication
+    }
 }
--- MVVMTraining/MVVMTraining/VIewModel/CalculateViewModel.swift
+++ MVVMTraining/MVVMTraining/VIewModel/CalculateViewModel.swift
@@ -8,13 +8,14 @@
 import SwiftUI
 
 class CalculateViewModel: ObservableObject{
+    let calculateModel = CalculateModel()
     @Published var fetchValue1 = ""
     @Published var fetchValue2 = ""
     //入力されたStringをIntに変換し、計算した結果をStringに返します
     var resrlt: String {
         let num1 = Int(fetchValue1) ?? 0
         let num2 = Int(fetchValue2) ?? 0
-        let result = String(calculate(num1: num1, num2: num2))
+        let result = String(calculateModel.calculate(num1: num1, num2: num2))
         return result
     }
 }
